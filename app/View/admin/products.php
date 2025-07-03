bfcd55b6217ae481efc2a86d4e4547e2eee61dfc
dichvietduc2005 <dvd192005@gmail.com>
2025-07-03 14:37:43 +0700

    sửa dropdown admin/product

--- app/View/admin/products.php
+++ app/View/admin/products.php
@@ -72,6 +72,8 @@ $pending_products = getPendingProducts($pdo);
         </table>
         <?php endif; ?>
     </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <?php footer(); ?>
 </body>
 
